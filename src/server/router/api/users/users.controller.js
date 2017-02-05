f1c7854b8912453f5af96db94a4c6de95787ab4c
syolin <wlgnsdoel@naver.com>
2017-02-05 15:01:37 +0900

    회원가입

--- src/server/router/api/users/users.controller.js
+++ src/server/router/api/users/users.controller.js
@@ -4,6 +4,7 @@ import crypto from 'crypto';
 
 const User = mongoose.model('User');
 
+// 유저 등록
 User.create = (username, userId, password, studentCode) => {
     const user = new User({
         username,
@@ -15,12 +16,14 @@ User.create = (username, userId, password, studentCode) => {
     return user.save();
 };
 
+// 유저 정보 검색
 User.findOneByUserId = userId => {
     return User.findOne({
         userId
     }).exec();
 };
 
+// Password Hash
 User.passwordHash = password => {
     const passwordHash = crypto.createHash("sha512").update(password).digest("hex");
 
