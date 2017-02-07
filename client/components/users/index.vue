5278de21450b4aadd76672db862b4333297c4881
bowwow <bowwow.bread@gmail.com>
2017-02-07 12:23:05 +0900

    회원가입 로그인

---
+++ client/components/users/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="index">
+        <div class="container">
+          <login v-if="sign"></login>
+          <register v-else="sign"></register>
+          <button type="button" name="button" v-on:click="change_sign">{{sign_msg}}</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import login from './login.vue'
+    import register from './register.vue'
+
+
+    // 변수에 넣어서 출력
+    export default {
+        name: 'index',
+        components: {
+            'login' : login,
+            'register': register
+        },
+        data () {
+            return {
+                sign : true,
+                sign_msg: "회원가입"
+            }
+        },
+        methods:{
+          change_sign : function(){
+            this.sign = !this.sign;
+            if(this.sign){
+              this.sign_msg = "회원가입"
+            }else{
+              this.sign_msg = "로그인"
+            }
+          }
+        }
+    }
+</script>
+
+<style scoped>
+    .container {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
