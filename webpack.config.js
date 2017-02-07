a3f2bec33c77c72923c1ccd851ec9bb7dc374584
bowwow <bowwow.bread@gmail.com>
2017-02-07 12:50:40 +0900

    users

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,56 @@
 var path = require('path')
 var webpack = require('webpack')
+require('es6-promise/auto');
 
 module.exports = {
-  entry: path.join(__dirname, 'src/client/index.js'),
-  output: {
-    path: path.join(__dirname, 'src/server/public/javascripts/'),
-    publicPath: '/javascripts/',
-    filename: 'build.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.vue']
-  },
-  resolveLoader: {
-    root: path.join(__dirname, 'node_modules')
-  },
-  module: {
-    preLoaders: [
-      {
-        test: /\.vue$/,
-        loader: 'eslint',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        loader: 'eslint',
-        exclude: /node_modules/
-      }
-    ],
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: { presets: ['es2015'] }
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url',
-        query: {
-          limit: 10000,
-          name: '[name].[ext]?[hash:7]'
-        }
-      }
+    entry: path.join(__dirname, 'src/client/index.js'),
+    output: {
+        path: path.join(__dirname, 'src/server/public/javascripts/'),
+        publicPath: '/javascripts/',
+        filename: 'build.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.vue']
+    },
+    resolveLoader: {
+        root: path.join(__dirname, 'node_modules')
+    },
+    module: {
+        loaders: [
+            {
+                //Awesome Swiper 로더 ? 등록
+                test: /\.css$/,
+                loaders: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {presets: ['es2015']}
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+            {
+                test: /\.(png|jpg|gif|svg|woff|ttf|woff2|eot)$/,
+                loader: 'url',
+                query: {
+                    limit: 10000,
+                    name: '[name].[ext]?[hash:7]'
+                }
+            }
+        ]
+    },
+    devtool: 'eval-source-map',
+    eslint: {
+        formatter: require('eslint-friendly-formatter')
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
     ]
-  },
-  devtool: 'eval-source-map',
-  eslint: {
-    formatter: require('eslint-friendly-formatter')
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
 }
