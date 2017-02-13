94b98f2768451218697498a735dcaeb553e3ca88
syolin <wlgnsdoel@naver.com>
2017-02-13 17:06:34 +0900

    last client upload

--- src/client/components/problems/problemlist.vue
+++ src/client/components/problems/problemlist.vue
@@ -1,34 +1,29 @@
 <template>
     <div class="problemlist">
         <!--example-->
-        <ul>
-            <li v-for="item in items">
-                - <span class="message">{{item.commit.message}}</span><br>
-                by <span class="author">{{item.commit.author.name}}</span><br>
-                at <span class="date">{{item.commit.author.date}}</span><br>
-                <hr>
-            </li>
-        </ul>
+        <!--<ul>-->
+        <!--<li v-for="item in items">-->
+        <!-- - <span class="message">{{item.commit.message}}</span><br>-->
+        <!--by <span class="author">{{item.commit.author.name}}</span><br>-->
+        <!--at <span class="date">{{item.commit.author.date}}</span><br>-->
+        <!--<hr>-->
+        <!--</li>-->
+        <!--</ul>-->
+
 
         <!--문제리스트-->
-        <!--<ul>-->
-            <!--{ list:-->
-               <!--[-->
-                   <!--{-->
-                        <!--num:'1234', problemname:'테스트문제01', source:'check', submission:-->
-                        <!--{success: '22', fail: '11', average: '50'}-->
-                   <!--}-->
-               <!--]-->
-            <!--}-->
-            <!--<li v-for="item in items">-->
-                <!--번호 : <span>{{item.list.num}}</span><br>-->
-                <!--이름 : <span>{{item.list.problemname}}</span><br>-->
-                <!--소스 : <span>{{item.list.check}}</span><br>-->
-                <!--성공 : <span>{{item.list.submission.success}}</span><br>-->
-                <!--실패 : <span>{{item.list.submission.fail}}</span><br>-->
-                <!--평균 : <span>{{item.list.submission.average}}</span><br>-->
-            <!--</li>-->
-        <!--</ul>-->
+        <ul>
+
+            <li v-for="item in items">
+                번호 : <span>{{item.num}}</span><br>
+                이름 : <span>{{item.name}}</span><br>
+                소스 : <span>{{item.source}}</span><br>
+                <br>
+            </li>
+            <br>
+
+        </ul>
+
 
 
         <!--해당문제만-->
@@ -55,7 +50,7 @@
         data(){
             return {
                 currentBranch: 'dev',
-                items: null,
+                items:[],
             }
         },
         created() {
@@ -63,21 +58,37 @@
         },
         methods: {
             //example
+//            fetchData: function () {
+//                this.$http.get('https://api.github.com/repositories/11730342/commits?per_page=5&sha=')
+//                    .then((res) => {
+//                        this.items = res.data;
+//                        console.log(res.data);
+//                    })
+//                this.$http.get('api/problems')
+//                    .then((res) => {
+//                        this.items = res.data;
+//                        console.log(res.data);
+//                    })
+
+//            }
+//            문제리스트
             fetchData: function () {
-                this.$http.get('https://api.github.com/repositories/11730342/commits?per_page=5&sha=')
-                    .then((response) => {
-                        this.items = response.data;
-                        console.log(response);
+                this.$http.get('api/problems')
+                    .then((res) => {
+//                        console.log(res.data.result);
+//                        console.log(res.data.problems[0]);
+
+                        var i = 0;
+                        while(i<res.data.problems.length) {
+                            this.items.push({
+                                num: res.data.problems[i].num,
+                                name: res.data.problems[i].problemName,
+                                source: res.data.problems[i].source,
+                            });
+                            i++;
+                        }
                     })
             }
-//            문제리스트
-//            fetchData: function () {
-//                this.$http.get('http://121.186.23.245:9999/api/problems')
-//                    .then((response)=> {
-//                        this.items = response.data;
-//                        console.log(response);
-//                    })
-//            }
 
             //해당문제만
 //            fetchData: function () {
@@ -87,7 +98,6 @@
 //                        console.log(response);
 //                    })
 //            }
-
 
 
         }
