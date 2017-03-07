4bb089cd0cd9b4b473619077885948c2c4d78682
syolin <wlgnsdoel@naver.com>
2017-03-08 02:57:02 +0900

    cors err5

--- src/server/index.js
+++ src/server/index.js
@@ -55,7 +55,7 @@ database.once('open', () => {
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     next();
 });
