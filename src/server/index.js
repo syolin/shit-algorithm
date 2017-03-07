b429b05393a4cc23ce2e02caf157c547c78d9ee4
syolin <wlgnsdoel@naver.com>
2017-03-08 02:33:56 +0900

    cors err4

--- src/server/index.js
+++ src/server/index.js
@@ -54,7 +54,7 @@ database.once('open', () => {
 });
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://121.186.23.245:9998");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     next();
