462b29184c1af947e0c618735d59acbb772860ff
syolin <wlgnsdoel@naver.com>
2017-03-08 02:13:10 +0900

    cors err2

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,7 @@ import webpack from 'webpack';
 import mongoose from 'mongoose';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import cors from 'cors';
 
 // router
 import router from './router/client/index';
@@ -53,17 +54,7 @@ database.once('open', () => {
     console.log('Connected to mongodb server');
 });
 
-var allowCORS = function(req, res, next) {
-    res.header('Acess-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    (req.method === 'OPTIONS') ?
-        res.send(200) :
-        next();
-};
-
-// 이 부분은 app.use(router) 전에 추가하도록 하자
-app.use(allowCORS);
+app.use(cors);
 
 
 // client router
