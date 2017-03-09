e032c74f0eb7e53a07073afa5027e596f7fd2dad
syolin <wlgnsdoel@naver.com>
2017-03-09 22:48:05 +0900

    fucking token cookie

--- src/server/modules/auth.js
+++ src/server/modules/auth.js
@@ -24,7 +24,7 @@ const auth = {
         return compose()
             .use((req, res, next) => {
                 // const token = req.cookies.userToken;
-                const token = req.cookies.userToken;
+                const token = req.headers.authorization;
                 if (token) {
                     const secret = req.app.get('jwt-secret');
                     try {
