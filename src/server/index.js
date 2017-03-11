704de75ec1a2265de9169c8d8f4cba4c4a9e7706
syolin <wlgnsdoel@naver.com>
2017-03-11 15:27:51 +0900

    Access-Control-Allow-Methods in preflight response.

--- src/server/index.js
+++ src/server/index.js
@@ -56,7 +56,7 @@ database.once('open', () => {
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
     next();
 });
 
