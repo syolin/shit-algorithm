acd9fca7ef9c34d2464fcb9118ef6a421345184e
세나 정 <wlgnsdoel@naver.com>
2017-03-16 02:38:56 +0900

    Delete config.js

--- src/server/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-            'secret': 'ShiTalgOriThMSerIvceSeCrETCodE',
-            'mongodbUri': 'mongodb://shitadmin:godqhrgks123@121.186.23.245:9990/shit',
-            'naverApiId': '',
-            'naverApiSecret':''
-};
