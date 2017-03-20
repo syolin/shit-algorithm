af3b8ddb7d42fbb7df8b9b9f2695aea5b1a92ac3
syolin <wlgnsdoel@naver.com>
2017-03-21 01:06:49 +0900

    console test exit3

--- src/server/router/api/problems/problems.js
+++ src/server/router/api/problems/problems.js
@@ -106,11 +106,10 @@ router.get('/:num',auth.isAuthenticated(), function (req, res) {
 
     const contest = problem => {
         const userData = userController.findOneByUserId(req.user.userId);
-        console.log("1 : "+userData);
-
-        if (problem.type == "contest" && !userData.contestAccount) throw new Error("아직 오픈되지 않았습니다.");
+        console.log("1 : "+userData.contestAccount);
+        console.log("1.5 :"+JSON.parse(userData).contestAccount);
         console.log("2 : "+problem.type+" , "+userData.contestAccount);
-
+        if (problem.type == "contest" && !userData.contestAccount) throw new Error("아직 오픈되지 않았습니다.");
         return problem;
     }
 
